1bc3112b721fb67d379667bb9acf5d4f61d16d3c
Kyle Barney <kyle.barney@gmail.com>
2026-06-28 20:23:35 -0400

    Add Directorate Registry

---
+++ backend/directorates/registry.py
@@ -0,0 +1,17 @@
+DIRECTORATES = {
+    "Commander": {},
+    "Engineering": {},
+    "Cyber Intelligence": {},
+    "Medical Intelligence": {},
+    "Financial Intelligence": {},
+    "Intelligence": {},
+    "Security": {},
+    "Legacy": {},
+    "Education": {},
+    "Operations": {},
+    "Communications": {},
+    "Logistics": {},
+    "Research": {},
+    "Family Office": {},
+    "Forge": {}
+}
